f9cdfa375f42acc5fa5ee3e49674116e0da8f297
Parth651-45 <85058684+Parth651-45@users.noreply.github.com>
2022-03-22 00:25:51 +0530

    Create __init__.py

---
+++ Tom/__init__.py
@@ -0,0 +1,40 @@
+
+
+from Tom.core.bot import TomBot
+from Tom.core.dir import dirr
+from Tom.core.git import git
+from Tom.core.userbot import Userbot
+from Tom.misc import dbb, heroku, sudo
+
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = TomBot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
