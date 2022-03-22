034e422dbecaeef457645b23337cb0c349fa6070
Parth651-45 <85058684+Parth651-45@users.noreply.github.com>
2022-03-22 15:46:34 +0530

    Update helpers.py

--- strings/helpers.py
+++ strings/helpers.py
@@ -76,6 +76,32 @@ HELP_4 = """⚠️<u>**𝙴𝚇𝚃𝚁𝙰 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂:**<
 /help  - 𝙶𝙴𝚃 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂 𝙷𝙴𝙻𝙿𝙴𝚁 𝙼𝙴𝙽𝚄 𝚆𝙸𝚃𝙷 𝙳𝙴𝚃𝙰𝙸𝙻𝙴𝙳 𝙴𝚇𝙿𝙻𝙰𝙽𝙰𝚃𝙸𝙾𝙽𝚂 𝙾𝙵 𝙲𝙾𝙼𝙼𝙰𝙽𝙳.
 /ping- 𝙿𝙸𝙽𝙶 𝚃𝙷𝙴 𝙱𝙾𝚃 𝙰𝙽𝙳 𝙲𝙷𝙴𝙲𝙺 𝚁𝙰𝙼, 𝙲𝙿𝚄 𝙴𝚃𝙲 𝚂𝚃𝙰𝚃𝚂 𝙾𝙵 𝙱𝙾𝚃.
 
+✅<u>**Group Settings:**</u>
+/settings - Get a complete group's settings with inline buttons
+
+🔗 **Options in Settings:**
+
+1️⃣ You can set **Audio Quality** you want to stream on voice chat.
+
+2️⃣ You can set **Video Quality** you want to stream on voice chat.
+
+3️⃣ **Auth Users**:- You can change admin commands mode from here to everyone or admins only. If everyone, anyone present in you group will be able to use admin commands(like /skip, /stop etc)
+
+4️⃣ **Clean Mode:** When enabled deletes the bot's messages after 5 mins from your group to make sure your chat remains clean and good.
+
+5️⃣ **Command Clean** : When activated, Bot will delete its executed commands (/play, /pause, /shuffle, /stop etc) immediately.
+
+6️⃣ **Play Settings:**
+
+/playmode - Get a complete play settings panel with buttons where you can set your group's play settings. 
+
+<u>Options in playmode:</u>
+
+1️⃣ **Search Mode** [Direct or Inline] - Changes your search mode while you give /play mode. 
+
+2️⃣ **Admin Commands** [Everyone or Admins] - If everyone, anyone present in you group will be able to use admin commands(like /skip, /stop etc)
+
+3️⃣ **Play Type** [Everyone or Admins] - If admins, only admins present in group can play music on voice chat."""
 
 HELP_5 = """🔰**<u>ADD & REMOVE SUDO USERS :</u>**
 /addsudo [Username or Reply to a user]
