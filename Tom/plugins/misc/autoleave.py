fc4fb3bef4dc6248e35e68ec2cdf30f318851bf7
Parth651-45 <85058684+Parth651-45@users.noreply.github.com>
2022-03-22 10:40:02 +0530

    Create autoleave.py

---
+++ Tom/plugins/misc/autoleave.py
@@ -0,0 +1,44 @@
+
+
+import asyncio
+
+import config
+from Tom.utils.database import get_client, is_active_chat
+
+
+async def auto_leave():
+    if config.AUTO_LEAVING_ASSISTANT == str(True):
+        while not await asyncio.sleep(
+            config.AUTO_LEAVE_ASSISTANT_TIME
+        ):
+            from YukkiMusic.core.userbot import assistants
+
+            for num in assistants:
+                client = await get_client(num)
+                try:
+                    async for i in client.iter_dialogs():
+                        chat_type = i.chat.type
+                        if chat_type in [
+                            "supergroup",
+                            "group",
+                            "channel",
+                        ]:
+                            chat_id = i.chat.id
+                            if (
+                                chat_id != config.LOG_GROUP_ID
+                                and chat_id != -1001661434960
+                                and chat_id != -1001699102242
+                                and chat_id != -1001609638358
+                            ):
+                                if not await is_active_chat(chat_id):
+                                    try:
+                                        await client.leave_chat(
+                                            chat_id
+                                        )
+                                    except:
+                                        continue
+                except:
+                    pass
+
+
+asyncio.create_task(auto_leave())
