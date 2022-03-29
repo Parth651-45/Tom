c428ec979f764b1ff9701172189bf5f8e278f0db
Parth651-45 <85058684+Parth651-45@users.noreply.github.com>
2022-03-29 20:50:07 +0530

    Update autoleave.py

--- Tom/plugins/misc/autoleave.py
+++ Tom/plugins/misc/autoleave.py
@@ -11,7 +11,7 @@ async def auto_leave():
         while not await asyncio.sleep(
             config.AUTO_LEAVE_ASSISTANT_TIME
         ):
-            from YukkiMusic.core.userbot import assistants
+            from Tom.core.userbot import assistants
 
             for num in assistants:
                 client = await get_client(num)
