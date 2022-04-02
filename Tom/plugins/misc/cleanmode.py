96671b39acbebc0a2d63a4d5e55490a0328ebb09
Parth651-45 <85058684+Parth651-45@users.noreply.github.com>
2022-04-02 18:28:14 +0530

    Update cleanmode.py

--- Tom/plugins/misc/cleanmode.py
+++ Tom/plugins/misc/cleanmode.py
@@ -156,7 +156,7 @@ async def braodcast_message(client, message, _):
     if "-assistant" in message.text:
         aw = await message.reply_text(_["broad_2"])
         text = _["broad_3"]
-        from YukkiMusic.core.userbot import assistants
+        from Tom.core.userbot import assistants
 
         for num in assistants:
             sent = 0
