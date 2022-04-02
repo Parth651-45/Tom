716ea6118f761a8609ff93c3dc92e364f65fa650
Parth651-45 <85058684+Parth651-45@users.noreply.github.com>
2022-04-02 12:52:13 +0530

    Update settings.py

--- Tom/plugins/bot/settings.py
+++ Tom/plugins/bot/settings.py
@@ -9,7 +9,7 @@ from config import (BANNED_USERS, CLEANMODE_DELETE_MINS,
                     MUSIC_BOT_NAME, OWNER_ID)
 from strings import get_command
 from Tom import app
-from Tom.utils.database import (is_suggestion, suggestion_off, suggestion_on, add_nonadmin_chat,
+from Tom.utils.database import (add_nonadmin_chat,
                                        cleanmode_off, cleanmode_on,
                                        commanddelete_off,
                                        commanddelete_on,
@@ -20,17 +20,19 @@ from Tom.utils.database import (is_suggestion, suggestion_off, suggestion_on, ad
                                        is_cleanmode_on,
                                        is_commanddelete_on,
                                        is_nonadmin_chat,
+                                       is_suggestion,
                                        remove_nonadmin_chat,
                                        save_audio_bitrate,
                                        save_video_bitrate,
-                                       set_playmode, set_playtype)
+                                       set_playmode, set_playtype,
+                                       suggestion_off, suggestion_on)
 from Tom.utils.decorators.admins import ActualAdminCB
 from Tom.utils.decorators.language import language, languageCB
 from Tom.utils.inline.settings import (
     audio_quality_markup, auth_users_markup,
     cleanmode_settings_markup, playmode_users_markup, setting_markup,
     video_quality_markup)
-from Tom.utils.inline.start import private_panel
+from YukkiMusic.utils.inline.start import private_panel
 
 ### Command
 SETTINGS_COMMAND = get_command("SETTINGS_COMMAND")
@@ -196,7 +198,9 @@ async def without_Admin_rights(client, CallbackQuery, _):
         sug = None
         if await is_suggestion(CallbackQuery.message.chat.id):
             sug = True
-        buttons = cleanmode_settings_markup(_, status=cle, dels=sta, sug=sug)
+        buttons = cleanmode_settings_markup(
+            _, status=cle, dels=sta, sug=sug
+        )
     if command == "AQ":
         try:
             await CallbackQuery.answer(_["set_cb_1"], show_alert=True)
@@ -487,7 +491,9 @@ async def authusers_mar(client, CallbackQuery, _):
 
 
 @app.on_callback_query(
-    filters.regex(pattern=r"^(CLEANMODE|COMMANDELMODE|SUGGESTIONCHANGE)$")
+    filters.regex(
+        pattern=r"^(CLEANMODE|COMMANDELMODE|SUGGESTIONCHANGE)$"
+    )
     & ~BANNED_USERS
 )
 @ActualAdminCB
@@ -510,7 +516,9 @@ async def cleanmode_mark(client, CallbackQuery, _):
         else:
             await cleanmode_on(CallbackQuery.message.chat.id)
             cle = True
-        buttons = cleanmode_settings_markup(_, status=cle, dels=sta, sug=sug)
+        buttons = cleanmode_settings_markup(
+            _, status=cle, dels=sta, sug=sug
+        )
         return await CallbackQuery.edit_message_reply_markup(
             reply_markup=InlineKeyboardMarkup(buttons)
         )
@@ -527,7 +535,9 @@ async def cleanmode_mark(client, CallbackQuery, _):
         else:
             await commanddelete_on(CallbackQuery.message.chat.id)
             sta = True
-        buttons = cleanmode_settings_markup(_, status=cle, dels=sta, sug=sug)
+        buttons = cleanmode_settings_markup(
+            _, status=cle, dels=sta, sug=sug
+        )
     if command == "SUGGESTIONCHANGE":
         cle = None
         sta = None
@@ -541,10 +551,15 @@ async def cleanmode_mark(client, CallbackQuery, _):
         else:
             await suggestion_on(CallbackQuery.message.chat.id)
             sug = True
-        buttons = cleanmode_settings_markup(_, status=cle, dels=sta, sug=sug)
+        buttons = cleanmode_settings_markup(
+            _, status=cle, dels=sta, sug=sug
+        )
     try:
         return await CallbackQuery.edit_message_reply_markup(
             reply_markup=InlineKeyboardMarkup(buttons)
         )
     except MessageNotModified:
         return
+
+
+        
