854bb914215c3fc5f2ecb0b3a7954fcc6211e1b8
Parth651-45 <85058684+Parth651-45@users.noreply.github.com>
2022-04-02 12:42:33 +0530

    Update queue.py

--- Tom/plugins/tools/queue.py
+++ Tom/plugins/tools/queue.py
@@ -223,8 +223,9 @@ async def queued_tracks(client, CallbackQuery: CallbackQuery, _):
         )
 
 
-
-@app.on_callback_query(filters.regex("queue_back_timer") & ~BANNED_USERS)
+@app.on_callback_query(
+    filters.regex("queue_back_timer") & ~BANNED_USERS
+)
 @languageCB
 async def queue_back(client, CallbackQuery: CallbackQuery, _):
     callback_data = CallbackQuery.data.strip()
@@ -294,10 +295,10 @@ async def queue_back(client, CallbackQuery: CallbackQuery, _):
     )
     basic[videoid] = True
 
-    med = InputMediaPhoto(
-        media=IMAGE, caption=cap
+    med = InputMediaPhoto(media=IMAGE, caption=cap)
+    mystic = await CallbackQuery.edit_message_media(
+        media=med, reply_markup=upl
     )
-    mystic = await CallbackQuery.edit_message_media(media=med, reply_markup=upl)
     if DUR != "Unknown":
         try:
             while db[chat_id][0]["vidid"] == videoid:
@@ -329,3 +330,4 @@ async def queue_back(client, CallbackQuery: CallbackQuery, _):
                     break
         except:
             return
+
